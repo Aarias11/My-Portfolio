a9bdb2b28d7a40b0758c01859fbe0c5c530cb3ba
Aarias11 <108088290+Aarias11@users.noreply.github.com>
2023-09-19 20:20:16 -0400

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,7 +9,7 @@ import MoneyPulsePic from '../assets/MoneyPulsePic.png'
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#1a1a1a]'>
+    <div name='work' className='w-full md:h-[1000px] text-gray-300 bg-[#1a1a1a]'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#3d808f]'>Work</p>
