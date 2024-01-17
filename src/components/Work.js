123d8f47c50e6371a0fd8c83e8530103826b7c4d
Aarias11 <108088290+Aarias11@users.noreply.github.com>
2024-01-17 17:08:37 -0500

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -16,45 +16,45 @@ const Work = () => {
                 <p className='py-6'>// Check out some of my recent work <br /> In this portfolio, you will find a collection of my projects, code samples, and other relevant information that demonstrates my skills and expertise. Each project showcases my ability to work with a range of programming languages, frameworks, and/or databases, and highlights my focus on delivering high-quality, efficient, and reliable software solutions.</p>
             </div>
 {/* Container */}
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                {/* Grid Item */}
-                <div style={{backgroundImage: `url(${MoneyPulsePic})`}}   className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+//             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+//                 {/* Grid Item */}
+//                 <div style={{backgroundImage: `url(${MoneyPulsePic})`}}   className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-React JS Application
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='https://moneypulseapp.web.app/' target="_blank">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='https://github.com/Aarias11/MoneyPulse-app' target="_blank">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
+//                     {/* Hover Effects */}
+//                     <div className='opacity-0 group-hover:opacity-100'>
+//                         <span className='text-2xl font-bold text-white tracking-wider'>
+// React JS Application
+//                         </span>
+//                         <div className='pt-8 text-center'>
+//                             <a href='https://moneypulseapp.web.app/' target="_blank">
+//                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+//                             </a>
+//                             <a href='https://github.com/Aarias11/MoneyPulse-app' target="_blank">
+//                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+//                             </a>
 
-                        </div>
-                    </div>
-                </div>
+//                         </div>
+//                     </div>
+//                 </div>
                 {/* Grid Item */}
-                <div style={{backgroundImage: `url(${FuseDB})`}}   className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+//                 <div style={{backgroundImage: `url(${FuseDB})`}}   className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-React JS Application
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='https://moviedatabase-79511.web.app' target="_blank">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='/' target="_blank">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
+//                     {/* Hover Effects */}
+//                     <div className='opacity-0 group-hover:opacity-100'>
+//                         <span className='text-2xl font-bold text-white tracking-wider'>
+// React JS Application
+//                         </span>
+//                         <div className='pt-8 text-center'>
+//                             <a href='https://moviedatabase-79511.web.app' target="_blank">
+//                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+//                             </a>
+//                             <a href='/' target="_blank">
+//                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+//                             </a>
 
-                        </div>
-                    </div>
-                </div>
+//                         </div>
+//                     </div>
+//                 </div>
                  {/* Grid Item */}
                  <div style={{backgroundImage: `url(${DashboardPic})`}}   className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
@@ -115,43 +115,43 @@ React JS Application
                 </div>
                 
                 {/* Grid Item */}
-                <div style={{backgroundImage: `url(${ledgerLiveproj})`}}   className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+//                 <div style={{backgroundImage: `url(${ledgerLiveproj})`}}   className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-React JS Application
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='/' target="_blank">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='https://github.com/Aarias11/Ledger-Clone' target="_blank">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
+//                     {/* Hover Effects */}
+//                     <div className='opacity-0 group-hover:opacity-100 '>
+//                         <span className='text-2xl font-bold text-white tracking-wider'>
+// React JS Application
+//                         </span>
+//                         <div className='pt-8 text-center'>
+//                             <a href='/' target="_blank">
+//                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+//                             </a>
+//                             <a href='https://github.com/Aarias11/Ledger-Clone' target="_blank">
+//                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+//                             </a>
 
-                        </div>
-                    </div>
-                </div>
+//                         </div>
+//                     </div>
+//                 </div>
                 {/* Grid Item */}
-                <div style={{backgroundImage: `url(${carsite})`}}   className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+//                 <div style={{backgroundImage: `url(${carsite})`}}   className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-React JS Application
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href='/' target="_blank">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href='/' target="_blank">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
+//                     {/* Hover Effects */}
+//                     <div className='opacity-0 group-hover:opacity-100'>
+//                         <span className='text-2xl font-bold text-white tracking-wider'>
+// React JS Application
+//                         </span>
+//                         <div className='pt-8 text-center'>
+//                             <a href='/' target="_blank">
+//                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+//                             </a>
+//                             <a href='/' target="_blank">
+//                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+//                             </a>
 
-                        </div>
-                    </div>
-                </div>
+//                         </div>
+//                     </div>
+//                 </div>
                
                 
                 {/* Grid Item */}
